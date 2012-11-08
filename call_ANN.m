6491b3ff54e51e2b73f1313bc3eb8310da0ea87a
Earl J. St Sauver <estsau01@tunic6.uit.tufts.edu>
2012-11-08 16:33:14 -0500

    Neural Network rough draft complete

--- call_ANN.m
+++ call_ANN.m
@@ -9,18 +9,41 @@ S=X;
 X(1) = 0.5;
 S(1) = 0.5;
 
-net = network
+net = network;
 net.numInputs = 3;
 net.numLayers = 2;
 net.biasConnect = [1; 1; 1];
 net.inputConnect(1,1)=1;
 net.inputConnect(1,2)=1;
 net.inputConnect(1,3)=1;
-net.layerConnect =[0 0;
+net.layerConnect =[1 1;
 					1 0;];
 net.outputConnect = [0 1];
 net.layers{1}.size = w_vector[1]
-net.layers{2}.size = w_vector[2]
+net.layers{1}.transferfcn = 'logsig';
+net.layers{1}.initFcn = 'initnw';
+net.layers{2}.size = w_vector[2];
+net.layers{2}.transferfcn = 'satlin';
+net.layers{2}.initFcn= 'initnw';
+net.inputConnect(1,1).delays = [0 1 2];
+net.inputConnect(1,2).delays = [0 1 2];
+net.inputConnect(1,3).delays = [0 1 2];
+net.layerConnect(1,1).delays = [1];
+net.layerConnect(1,2).delays = [1 2];
+net.initFcn = 'initlay';
+
+net.performFcn='mse';
+net.trainFcn= 'trainlm';
+net.divideFcn = 'dividerand';
+net.plotFcns = {'plotperform' , 'plottrainstate'};
+
+
+net = init(net);
+T = sim_data;
+net = train(net,F,T);
+
+
+
 for t = 2:N_t;
     Z0 = [F(t-1); X(t-1); S(t-1); 1];
     Z1 = W1*Z0;
